ffa3afdc6c426e961421125896de7b5547f2000d
rajeshpeddalachugari <rajesh.peddalachugari@gmail.com>
2016-07-09 18:46:31 +0530

    major commit

--- js/javascript.js
+++ js/javascript.js
@@ -1,17 +1,45 @@
-var myBtn = document.getElementsByClassName("tablink")[0];
-    myBtn.click();
-
 function openCity(evt, cityName) {
   var i, x, tablinks;
-  x = document.getElementsByClassName("city");
+  x = document.getElementsByClassName("albumYear");
   for (i = 0; i < x.length; i++) {
      x[i].style.display = "none";
   }
   tablinks = document.getElementsByClassName("tablink");
   for (i = 0; i < x.length; i++) {
-     tablinks[i].className = tablinks[i].className.replace(" w3-border-red", "");
+     tablinks[i].className = tablinks[i].className.replace(" w3-border-blue", "");
   }
   document.getElementById(cityName).style.display = "block";
-  evt.currentTarget.firstElementChild.className += " w3-border-red";
+  evt.currentTarget.firstElementChild.className += " w3-border-blue";
 }
-    
+    var myBtn = document.getElementsByClassName("tablink")[0];
+    myBtn.click();
+
+//
+
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+     dots[i].className = dots[i].className.replace(" w3-red", "");
+  }
+  x[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " w3-red";
+}
