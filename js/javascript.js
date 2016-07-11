73daa80da0802ff1ac1cdf34b6ead7a58e0736cd
rajeshpeddalachugari <rajesh.peddalachugari@gmail.com>
2016-07-11 21:57:41 +0530

    slide show fix

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,11 @@
+$(window).load(function() {
+		// Animate loader off screen
+		$(".se-pre-con").fadeOut("slow");;
+	});
+
+
+
+
 function openCity(evt, cityName) {
   var i, x, tablinks;
   x = document.getElementsByClassName("albumYear");
@@ -18,19 +26,21 @@ function openCity(evt, cityName) {
 
 
 var slideIndex = 1;
-showDivs(slideIndex);
+//showDivs(slideIndex,'mod1');
 
-function plusDivs(n) {
-  showDivs(slideIndex += n);
+function plusDivs(n,parent) {
+  showDivs(slideIndex += n,parent);
 }
 
-function currentDiv(n) {
-  showDivs(slideIndex = n);
+function currentDiv(n,parent) {
+  showDivs(slideIndex = n,parent);
 }
 
-function showDivs(n) {
+function showDivs(n,parent) {
   var i;
-  var x = document.getElementsByClassName("mySlides");
+    var x = $('.'+parent).find(".mySlides");
+    
+  //var x = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("demo");
   if (n > x.length) {slideIndex = 1}
   if (n < 1) {slideIndex = x.length}
