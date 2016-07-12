1d03d022cac191df33e79f62a659d12f075a1cec
rajeshpeddalachugari <rajesh.peddalachugari@gmail.com>
2016-07-12 16:55:22 +0530

    home page

--- js/javascript.js
+++ js/javascript.js
@@ -10,23 +10,42 @@ $(window).load(function () {
 
 
 //hello gallery tab
-function openCity(evt, cityName) {
+function openYear(evt, cityName) {
     var i, x, tablinks;
     x = document.getElementsByClassName("albumYear");
     for (i = 0; i < x.length; i++) {
         x[i].style.display = "none";
     }
-    tablinks = document.getElementsByClassName("tablink");
+    tablinks = document.getElementsByClassName("yearTab");
     for (i = 0; i < x.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" w3-border-blue", "");
     }
     document.getElementById(cityName).style.display = "block";
     evt.currentTarget.firstElementChild.className += " w3-border-blue";
 }
-var myBtn = document.getElementsByClassName("tablink")[0];
-myBtn.click();
+//var myBtn = document.getElementsByClassName("yearTab")[0];
+//myBtn.click();
 
 //end of gallery tab
+
+//
+
+function openDivision(evt, cityName) {
+    var i, x, tablinks;
+    x = document.getElementsByClassName("division");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("Divisionlink");
+    for (i = 0; i < x.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" w3-border-red", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.firstElementChild.className += " w3-border-red";
+}
+
+//
+
 
 //slideshow
 var slideIndex = 1;
