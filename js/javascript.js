889be32bbed0c6d79ecdc5afb5eab34faa8bd093
rajeshpeddalachugari <rajesh.peddalachugari@gmail.com>
2016-07-14 00:40:42 +0530

    cards update

--- js/javascript.js
+++ js/javascript.js
@@ -38,10 +38,10 @@ function openDivision(evt, cityName) {
     }
     tablinks = document.getElementsByClassName("Divisionlink");
     for (i = 0; i < x.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" w3-border-red", "");
+        tablinks[i].className = tablinks[i].className.replace(" w3-border-blue", "");
     }
     document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.firstElementChild.className += " w3-border-red";
+    evt.currentTarget.firstElementChild.className += " w3-border-blue";
 }
 var myBtn = document.getElementsByClassName("Divisionlink")[0];
 myBtn.click();
